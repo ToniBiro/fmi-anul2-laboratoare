b4cb21e63064eebf96bade497c1da5cd5f3097d7
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2019-10-27 20:38:49 +0200

    2 ex for lab4

--- TAP/laborator/lab4/lab4_ex1.py
+++ TAP/laborator/lab4/lab4_ex1.py
@@ -1,3 +1,48 @@
 # inversiuni
 # inversiuni semnificative
 
+with open("vector.in", 'r') as fin:
+    data = next(fin).split()
+
+
+v = []
+for elem in data:
+    v.append(int(elem))
+
+
+for idx in range(len(v)//2, len(v)):
+    v[idx] = 2*v[idx]
+
+print(v)
+
+def inv(st, dr):
+    if st == dr:
+        return 0
+    s = [] #v[st:dr+1] sortat
+    m = (st + dr)//2
+    nrs = inv(st, m)
+    nrd = inv(m + 1, dr)
+    vs = v[st:m+1] #subvectorul stang (2, 3, 6, 9, 10)
+    vd = v[m+1:dr+1] #subvectorul drept (1, 4, 5, 7, 8)
+    nrm = 0 #numarul de inv (a,b) a din vs, b din vd
+    i, j = 0, 0 #se interclaseaza in s vectorii vs si vd
+    while i < len(vs) and j < len(vd):
+        if vs[i] < vd[j]:
+            s.append(vs[i])
+            nrm += j #nr de valori din vd mai mici dect vs[i]
+            i += 1
+        else:
+            s.append(vd[j])
+            j += 1
+    while i < len(vs):
+        s.append(vs[i])
+        nrm += j
+        i += 1
+    while j < len(vd):
+        s.append(vd[j])
+        j += 1
+    v[st:dr+1] = s
+    return nrs + nrd + nrm
+
+
+print(inv(0, len(v)-1))
--- TAP/laborator/lab4/lab4_ex2.py
+++ TAP/laborator/lab4/lab4_ex2.py
@@ -14,29 +14,25 @@ print(interogari)
 t_dim_2 = [[1, 2], [3, 4]]
 
 
-def tabla(dim, i, j, plus):
+def tabla(dim, i, j):
+    total = dim**2
     if dim == 2:
-        print(f"plus: {plus}")
-        return t_dim_2[i-1][j-1] + plus
+        return t_dim_2[i-1][j-1]
     if i <= dim//2:
         if j <= dim//2:  #cadran I
-            print(f"plus: {plus}")
-            return tabla(dim//2, i, j, plus)
+            return tabla(dim//2, i, j)
         else:           #cadran II
             j -= dim//2
-            print(f"plus: {plus}")
-            return tabla(dim//2, i, j, plus + 2**(dim//2))
+            return total//4 + tabla(dim//2, i, j)
     else:
         if j <= dim//2:  #cadran III
             i -= dim//2
-            print(f"plus: {plus}" )
-            return tabla(dim//2, i, j, plus + 2**(dim//2 + 1))
+            return total//2 + tabla(dim//2, i, j)
         else:           #cadran IV
             i -= dim//2
             j -= dim//2
-            print(f"plus: {plus}" )
-            return tabla(dim//2, i, j, plus + 2**(dim//2 + 2))
+            return 3*total//4 + tabla(dim//2, i, j)
 
 with open("z.out", 'w') as fout:
     for interogare in interogari:
-        print(tabla(2**n, interogare[0], interogare[1], 0), file=fout)
+        print(tabla(2**n, interogare[0], interogare[1]), file=fout)
