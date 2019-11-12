78cc5c888859e6a657542477755af988a076039b
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2019-11-12 09:33:16 +0200

    Update lab5_ex3.py

--- TAP/laborator/lab5/lab5_ex3.py
+++ TAP/laborator/lab5/lab5_ex3.py
@@ -7,43 +7,48 @@ with open("cmap.in", 'r') as fin:
     for i in range(n):
         puncte.append([int(elem) for elem in next(fin).split()])
 
-X = [elem[0] for elem in puncte]
-Y = [elem[1] for elem in puncte]
+X = puncte.copy()
+Y = puncte.copy()
 X.sort()
-Y.sort()
+Y.sort(key = lambda elem: elem[1], reverse=False)
+print(Y)
 
 
 def dist(x1, y1, x2, y2):
     return sqrt((x1-x2)**2 + (y1-y2)**2)
 
 
-def min_dist(vect, s, f, Y):
+def min_dist(vect, s, f, Y_):
     if s >= f:
         return INF
     if f-s == 1:
         return dist(vect[s][0], vect[s][1], vect[f][0], vect[f][1])
 
     mij = (s + f)//2
-    m =X[mij][0]
+    m = X[mij][0]
 
-
+    YS = [elem for elem in Y_ if elem[0] <= m]
+    YD = [elem for elem in Y_ if elem[0] > m]
+    print(f"mij: {mij}   len(YS) {len(YS)} len(YD) {len(YD)}")
     d_s = min_dist(vect, s, mij-1, YS)
-    d_d = min_dist(vect, mij+1, f),YD
+    d_d = min_dist(vect, mij+1, f, YD)
 
     d = min(d_s, d_d)
+    print(f"d:{d}")
 
-    y_sort = [elem for elem in Y[s:f+1] if abs(elem[0] - vect[mij][0]) < d]
-    y_sort.sort(key=lambda elem: elem[1], reverse=False)
+    y_sort = [elem for elem in Y_ if m + d >= elem[0] and m - d <= elem[0]]
+    print(y_sort)
 
     for idx, elem in enumerate(y_sort):
-        for i in range(1, len(y_sort)-idx):
-            index = (idx+i)
-            d = min(dist(elem[0], elem[1], y_sort[index][0], y_sort[index][1]), d)
+        for i in range(1, 8):
+            if idx+i < len(y_sort):
+                d = min(dist(elem[0], elem[1], y_sort[idx+i][0], y_sort[idx+i][1]), d)
 
     return d
 
+
 with open("cmap.out", 'w') as fout:
-    print(min_dist(puncte, 0, len(puncte)-1), file=fout)
+    print(min_dist(puncte, 0, len(puncte)-1, Y), file=fout)
 
 
 #print(f"distanta minima rezultata = {min_dist(puncte, 0, len(puncte)-1)}")
