1eb5524f97fd6c733f31bed75b8351fe108777d7
ToniBiro <33375662+ToniBiro@users.noreply.github.com>
2019-11-27 10:25:32 +0200

    proiect TW

--- TW/JavaScrisp/my_first_JS.js
+++ TW/JavaScrisp/my_first_JS.js
@@ -3,18 +3,18 @@ window.onload = function () {
 
     // var x = document.getElementById('paragraf_1');
     //
-    // x.onclick = function () {
-    //     var img = document.createElement("img");
-    //     img.src = "5.png"
-    //     document.body.appendChild(img);
-    //
-    //     img.style.width = "20%";
-    //     img.style.height = "auto";
-    //
-    //
-    //     img.onclick = function () {
-    //     document.body.removeChild(img);
-    //     }
+    x.onclick = function () {
+        var img = document.createElement("img");
+        img.src = "5.png"
+        document.body.appendChild(img);
+
+        img.style.width = "20%";
+        img.style.height = "auto";
+
+
+        img.onclick = function () {
+        document.body.removeChild(img);
+        }
     // }
     //
     // var txn = document.createTextNode("___first text node___");
